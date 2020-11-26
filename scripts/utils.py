005bf8d02ca522e0ccd30dd2c44ef817f6e9783b
Ethan Kruse <6944089+ethankruse@users.noreply.github.com>
2020-11-25 22:20:59 -0500

    fix a wasp

--- scripts/utils.py
+++ scripts/utils.py
@@ -1639,7 +1639,7 @@ def load_data(updated_koi_params=True, updated_k2_params=True, new=True):
                 'TOI-1339.01', 'TOI-1339.02', 'TOI-1462.01', 'TOI-1728.01',
                 'TOI-1690.01', 'TOI-193.01', 'TOI-824.01', 'TOI-1339.03',
                 'TOI-421.01', 'TOI-540.01', 'TOI-1266.01', 'TOI-1266.02',
-                'TOI-488.01', 'TOI-837.01', 'TOI-954.01', 'TOI-2403.01', 
+                'TOI-488.01', 'TOI-837.01', 'TOI-954.01',  
                 'TOI-2410.01']
     tbc = np.zeros(len(tobeconf), dtype=bool)
     # single transits that should be set as confirmed
