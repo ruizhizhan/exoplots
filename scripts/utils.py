9c1c68db4334212492847c9656eda5b5cc03dd69
Ethan Kruse <6944089+ethankruse@users.noreply.github.com>
2020-12-17 13:33:03 -0500

    bunch of changes and new planets

--- scripts/utils.py
+++ scripts/utils.py
@@ -1587,14 +1587,19 @@ def load_data(updated_koi_params=True, updated_k2_params=True, new=True):
                'NGTS-11 b']
     # we know what these are and they have paper trails of submitted papers
     # though some were submitted way back in 2014 and still in limbo
-    # only TOI-1918/481/892 I can't figure out what it is yet
+    # some are newly submitted and waiting to be accepted but are
+    # prematurely marked confirmed on ExoFOP
     waiting = ['TOI-126.01', 'TOI-143.01', 'TOI-257.01',
                'TOI-295.01', 'TOI-626.01', 'TOI-657.01',
                'TOI-834.01', 'TOI-840.01', 'TOI-857.01',
-               'TOI-1071.01', 'TOI-1493.01', 'TOI-1494.01', 'TOI-1580.01',
+               'TOI-1071.01', 'TOI-1580.01',
                'TOI-1603.01', 'TOI-1826.01',
-               'TOI-1918.01', 'TOI-481.01', 'TOI-892.01',
-               'TOI-2179.01', 'TOI-2330.01', 'TOI-201.01', 'TOI-942.01']
+               'TOI-1918.01', 'TOI-892.01',
+               'TOI-2179.01', 'TOI-2330.01', 'TOI-201.01', 
+               'TOI-261.02', 'TOI-262.01', 'TOI-469.01', 'TOI-561.01',
+               'TOI-561.02', 'TOI-682.01', 'TOI-836.01', 'TOI-1054.01',
+               'TOI-1203.01', 'TOI-1230.01', 'TOI-1233.01', 'TOI-1239.01',
+               'TOI-1774.01']
 
     stillbad = np.zeros(len(ignores), dtype=bool)
     stillwaiting = np.zeros(len(waiting), dtype=bool)
@@ -1646,11 +1651,12 @@ def load_data(updated_koi_params=True, updated_k2_params=True, new=True):
                 'TOI-294.01', 'TOI-448.01', 'TOI-652.01', 
                 'TOI-704.01', 'TOI-732.01',
                 'TOI-732.02', 'TOI-736.01', 'TOI-736.02', 'TOI-1078.01',
-                'TOI-1339.01', 'TOI-1339.02', 'TOI-1462.01', 'TOI-1728.01',
-                'TOI-1690.01', 'TOI-193.01', 'TOI-824.01', 'TOI-1339.03',
+                'TOI-1462.01', 'TOI-1728.01',
+                'TOI-1690.01', 'TOI-193.01', 'TOI-824.01',
                 'TOI-421.01', 'TOI-540.01', 'TOI-1266.01', 'TOI-1266.02',
                 'TOI-488.01', 'TOI-837.01', 'TOI-954.01',  
-                'TOI-2410.01']
+                'TOI-2410.01', 'TOI-122.01', 'TOI-237.01', 'TOI-251.01',
+                'TOI-442.01']
     tbc = np.zeros(len(tobeconf), dtype=bool)
     # single transits that should be set as confirmed
     nopermatch = []
