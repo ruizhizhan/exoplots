945223343dcbbac07577311b1b487170a7c486e6
Ethan Kruse <6944089+ethankruse@users.noreply.github.com>
2021-06-18 12:26:55 -0400

    lots of new kepler confirmed

--- scripts/utils.py
+++ scripts/utils.py
@@ -634,8 +634,8 @@ def load_data(updated_koi_params=True, updated_k2_params=True, new=True):
 
     # these are now confirmed but they didn't update it
     # in the KOI table
-    newconf = []
-    newconf2 = []
+    newconf = ['KOI-1831.03', 'KOI-3503.01', 'KOI-3503.02', 'KOI-1833.02']
+    newconf2 = ['Kepler-324 d', 'Kepler-1703 b', 'Kepler-1703 c', 'Kepler-968 d']
     isconf = np.zeros(len(newconf), dtype=bool)
 
     # make sure all candidate KOIs aren't in the confirmed table
@@ -1592,10 +1592,10 @@ def load_data(updated_koi_params=True, updated_k2_params=True, new=True):
     # 351 TESS got the period wrong by 2x
     ignores = ['TOI-1338.01', 'TOI-1456.01',
                'TOI-1793.01', 'TOI-1899.01', 'TOI-2011.01', 'TOI-2221.01',
-               'TOI-351.01', 'TOI-1847.01', 'TOI-704.01']
+               'TOI-351.01', 'TOI-1847.01', 'TOI-704.01', 'TOI-2319.01']
     conname = ['TOI-1338 b', 'HD 332231 b', 'HD 95338 b',
                'TOI-1899 b', 'HD 136352 b', 'AU Mic b', 'WASP-165 b',
-               'NGTS-11 b', 'LHS 1815 b']
+               'NGTS-11 b', 'LHS 1815 b', 'HD 152843 c']
     # we know what these are and they have paper trails of submitted papers
     # though some were submitted way back in 2014 and still in limbo
     # some are newly submitted and waiting to be accepted but are
@@ -1608,7 +1608,7 @@ def load_data(updated_koi_params=True, updated_k2_params=True, new=True):
                'TOI-682.01', 'TOI-836.01', 'TOI-1054.01', 'TOI-1203.01',
                'TOI-1230.01', 'TOI-1239.01', 'TOI-1774.01', 'TOI-178.02',
                'TOI-558.01', 'TOI-559.01', 'TOI-263.01', 
-               'TOI-1634.01', 'TOI-674.01', 'TOI-2707.01', 'TOI-2319.01']
+               'TOI-1634.01', 'TOI-674.01', 'TOI-2707.01']
 
     stillbad = np.zeros(len(ignores), dtype=bool)
     stillwaiting = np.zeros(len(waiting), dtype=bool)
