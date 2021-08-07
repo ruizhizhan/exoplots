fade7e663be25d230843a07720462a556608997f
Ethan Kruse <6944089+ethankruse@users.noreply.github.com>
2021-08-06 22:34:09 -0400

    new cp

--- scripts/utils.py
+++ scripts/utils.py
@@ -1607,7 +1607,7 @@ def load_data(updated_koi_params=True, updated_k2_params=True, new=True):
                'TOI-682.01', 'TOI-836.01', 'TOI-1054.01', 'TOI-1203.01',
                'TOI-1230.01', 'TOI-1239.01', 'TOI-1774.01', 'TOI-178.02',
                'TOI-558.01', 'TOI-559.01', 'TOI-263.01', 
-               'TOI-3422.01']
+               'TOI-3422.01', 'TOI-431.01', 'TOI-431.02']
 
     stillbad = np.zeros(len(ignores), dtype=bool)
     stillwaiting = np.zeros(len(waiting), dtype=bool)
