e047b41b5b5809c1d2cd934f65b7c8ba0a461c67
Ethan Kruse <6944089+ethankruse@users.noreply.github.com>
2021-11-03 15:21:18 -0400

    tess finding KOIs

--- scripts/utils.py
+++ scripts/utils.py
@@ -1692,7 +1692,8 @@ def load_data(updated_koi_params=True, updated_k2_params=True, new=True):
                 'TOI-1749.02', 'TOI-3705.01', 'TOI-1062.01', 'TOI-532.01',
                 'TOI-4411.01', 'TOI-1518.01', 'TOI-4433.01', 'TOI-4444.01',
                 'TOI-4484.01', 'TOI-1431.01', 'TOI-421.02', 'TOI-509.01',
-                'TOI-509.02', 'TOI-1789.01', 'TOI-3362.01', 'TOI-4540.01']
+                'TOI-509.02', 'TOI-1789.01', 'TOI-3362.01', 'TOI-4540.01',
+                'TOI-4587.01', 'TOI-4588.01', 'TOI-4593.01', 'TOI-4594.01']
     tobeadded = []
     tbc = np.zeros(len(tobeconf), dtype=bool)
     # single transits that should be set as confirmed
