629c10025de4a91aa0c91c2282d0381612fc7315
Ethan Kruse <6944089+ethankruse@users.noreply.github.com>
2022-04-06 14:26:37 -0400

    new planets

--- scripts/utils.py
+++ scripts/utils.py
@@ -1634,7 +1634,7 @@ def load_data(updated_koi_params=True, updated_k2_params=True):
                 'TOI-1075.01', 'TOI-1268.01', 'TOI-1411.01', 'TOI-1693.01',
                 'TOI-1860.01', 'TOI-2260.01', 'TOI-2337.01', 'TOI-2411.01',
                 'TOI-2427.01', 'TOI-2669.01', 'TOI-4329.01', 'TOI-1670.01',
-                'TOI-1670.02', 'TOI-2257.01',
+                'TOI-1670.02', 'TOI-2257.01', 'TOI-1842.01', 'TOI-2180.01',
                 # KOIs
                 'TOI-4433.01', 'TOI-4444.01', 'TOI-4484.01', 'TOI-4588.01',
                 # K2 candidates
@@ -1648,8 +1648,8 @@ def load_data(updated_koi_params=True, updated_k2_params=True):
     tobeadded = []
     tbc = np.zeros(len(tobeconf), dtype=bool)
     # single transits that should be set as confirmed
-    nopermatch = ['TOI-2180.01']
-    confmatch = ['TOI-2180 b']
+    nopermatch = []
+    confmatch = []
     singconf = np.zeros(len(nopermatch), dtype=bool)
 
     # any candidates in the confirmed table get set as such
