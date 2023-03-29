493e4fde0433c0306659bcfe8f4e2a7cd96423e3
Ethan Kruse <6944089+ethankruse@users.noreply.github.com>
2023-03-29 16:10:35 -0400

    some early planets

--- scripts/utils.py
+++ scripts/utils.py
@@ -1497,7 +1497,8 @@ def load_data(updated_koi_params=True, only_candidates=True):
                'TOI-263.01', 'TOI-3422.01', 'TOI-3666.01', 'TOI-5153.01',
                'TOI-4406.01', 'TOI-5812.01', 'TOI-1260.03', 'TOI-2338.01',
                'TOI-2589.01', 'TOI-3984.01', 'TOI-5293.01', 'TOI-4603.01',
-               'TOI-244.01', 'TOI-6101.01']
+               'TOI-244.01', 'TOI-6101.01',
+               'TOI-615.01', 'TOI-622.01', 'TOI-2641.01', 'TOI-4127.01']
     earlycps = []
 
     stillbad = np.zeros(len(ignores), dtype=bool)
