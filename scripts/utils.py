641883f8552ecf8c042b3d9474f5f378af1f4964
Ethan Kruse <6944089+ethankruse@users.noreply.github.com>
2023-04-27 23:17:27 -0400

    new kps

--- scripts/utils.py
+++ scripts/utils.py
@@ -1556,6 +1556,7 @@ def load_data(updated_koi_params=True, only_candidates=True):
                 'TOI-3807.01', 'TOI-3819.01', 'TOI-3912.01', 'TOI-3976.01',
                 'TOI-4087.01', 'TOI-4145.01', 'TOI-4463.01', 'TOI-4791.01',
                 'TOI-2096.01', 'TOI-2096.02', 'TOI-5557.01',
+                'TOI-6273.01', 'TOI-6278.01',
                 # KOIs
                 'TOI-4444.01', 'TOI-4484.01', 'TOI-4588.01', 'TOI-1241.01',
                 # K2 candidates
