dfbe56a471002e926ed3d338fb1c2400be67f0f2
Ethan Kruse <6944089+ethankruse@users.noreply.github.com>
2023-07-14 23:18:04 -0400

    early

--- scripts/utils.py
+++ scripts/utils.py
@@ -1503,7 +1503,8 @@ def load_data(updated_koi_params=True, only_candidates=True):
                'TOI-5812.01', 'TOI-1260.03', 'TOI-3984.01', 'TOI-5293.01',
                'TOI-6101.01', 'TOI-615.01', 'TOI-622.01', 'TOI-2641.01',
                'TOI-6170.01', 'TOI-3785.01', 'TOI-2095.01', 'TOI-2095.02',
-               'TOI-1471.02', 'TOI-1470.01', 'TOI-5678.01', 'TOI-696.03']
+               'TOI-1471.02', 'TOI-1470.01', 'TOI-5678.01', 'TOI-696.03',
+               'TOI-4201.01']
     earlycps = []
 
     stillbad = np.zeros(len(ignores), dtype=bool)
