4d372e693967c69ca0760152370c962ae27b086d
Ethan Kruse <6944089+ethankruse@users.noreply.github.com>
2023-08-01 22:12:09 -0400

    archive bug fix

--- scripts/utils.py
+++ scripts/utils.py
@@ -522,10 +522,8 @@ def load_data(updated_koi_params=True, only_candidates=True):
     assert (~np.isfinite(dfcon['Jmag']) | (dfcon['Jmag'] > -5)).all()
 
     # RA and Dec are both valid
-    tmp = (dfcon['ra'] >= 0) & (dfcon['ra'] <= 360.)
-    assert tmp.sum() == tmp.size - 1
-    # assert ((dfcon['ra'] >= 0) & (dfcon['ra'] <= 360.)).all()
-    # assert ((dfcon['dec'] >= -90) & (dfcon['dec'] <= 90.)).all()
+    assert ((dfcon['ra'] >= 0) & (dfcon['ra'] <= 360.)).all()
+    assert ((dfcon['dec'] >= -90) & (dfcon['dec'] <= 90.)).all()
 
     # planet parameters are either NaN or > 0
     assert (~np.isfinite(dfcon['period']) | (dfcon['period'] > 0)).all()
